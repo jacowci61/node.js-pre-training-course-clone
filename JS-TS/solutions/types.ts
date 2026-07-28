9905a0eb1ab13a347fbfcefe134e928e5447a4e1
jacowci61 <jacowci61@gmail.com>
2026-07-28 21:40:36 +0300

    Completed task n1

--- JS-TS/solutions/types.ts
+++ JS-TS/solutions/types.ts
@@ -1,9 +1,15 @@
 interface Todo {
-
+    id : number;
+    title: string;
+    description?: string;
+    status: TodoStatus;
+    readonly createdAt: Date
 }
+
+type NewTodo = Omit<Todo, 'id' | 'CreatedAt'>
 
 enum TodoStatus {
-
+    PENDING, IN_PROGRESS, COMPLETED
 }
 
-export { Todo, TodoStatus };
+export { Todo, TodoStatus, NewTodo };
