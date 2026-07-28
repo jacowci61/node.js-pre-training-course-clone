c1095e7118cd3179beaed755c9a68af42dc11e07
jacowci61 <jacowci61@gmail.com>
2026-07-28 21:39:03 +0300

    Completed task n2

--- JS-TS/solutions/array-helpers.ts
+++ JS-TS/solutions/array-helpers.ts
@@ -3,21 +3,89 @@
 // All helpers are declared but not implemented.
 
 export function mapArray<T, R>(source: readonly T[], mapper: (item: T, index: number) => R): R[] {
-  throw new Error('mapArray: not implemented');
+    if (source === null || source === undefined){
+    throw new TypeError('Input is null/undefined.')
+  }
+
+  const dynamicArray: R[] = []; 
+  let i = 0;
+  for (const element of source) {
+      dynamicArray.push(mapper(element, i));
+      i += 1;
+  }
+  return dynamicArray
 }
 
 export function filterArray<T>(source: readonly T[], predicate: (item: T, index: number) => boolean): T[] {
-  throw new Error('filterArray: not implemented');
+      if (source === null || source === undefined){
+    throw new TypeError('Input is null/undefined.')
+  }
+  
+  const dynamicArray: T[] = []; 
+  let i = 0;
+
+  for (const element of source) {
+      if (predicate(element, i)){
+        dynamicArray.push(element);
+      }
+      i += 1;
+  }
+  return dynamicArray
 }
 
 export function reduceArray<T, R>(source: readonly T[], reducer: (acc: R, item: T, index: number) => R, initial: R): R {
-  throw new Error('reduceArray: not implemented');
+    if (source === null || source === undefined){
+    throw new TypeError('Input is null/undefined.')
+  }
+
+  let i = 0;
+  let acc: R = initial;
+
+  for (const element of source) {
+      acc = reducer(acc, element, i)
+      i += 1;
+  }
+
+  return acc;
 }
 
 export function partition<T>(source: readonly T[], predicate: (item: T) => boolean): [T[], T[]] {
-  throw new Error('partition: not implemented');
+    if (source === null || source === undefined){
+    throw new TypeError('Input is null/undefined.')
+  }
+
+  const dynamicArrayTrue: T[] = []; 
+  const dynamicArrayFalse: T[] = []; 
+
+  for (const element of source) {
+      if (predicate(element)){
+        dynamicArrayTrue.push(element);
+      }
+      else{
+        dynamicArrayFalse.push(element)
+      }
+  }
+
+  const result: [T[], T[]] = [dynamicArrayTrue, dynamicArrayFalse];
+
+  return result
 }
 
 export function groupBy<T, K extends PropertyKey>(source: readonly T[], keySelector: (item: T) => K): Record<K, T[]> {
-  throw new Error('groupBy: not implemented');
+      if (source === null || source === undefined){
+    throw new TypeError('Input is null/undefined.')
+  }
+
+
+  const result: Partial<Record<K, T[]>> = {};
+
+  for (const element of source){
+    const key = keySelector(element)
+    
+    if (!(key in result)) {
+      result[key] = [];
+    }
+    result[key]!.push(element);
+  }
+  return result as Record<K, T[]>;
 }
