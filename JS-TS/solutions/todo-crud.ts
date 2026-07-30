3a0213581a5e68aeb3349ea21c7791ec8542712b
jacowci61 <jacowci61@gmail.com>
2026-07-30 16:04:50 +0300

    Add implementations except updateTodo and checks for non-existent id

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,25 @@
 import { Todo } from './types';
 
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+  const copyOfState: Todo[] = [...state];
+  copyOfState.push(todo);
+  return copyOfState
 }
 
 export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
+  const extractedTodo = state.find(item => item.id === id);
+  const updatedTodo = update;
+  type partialTodo = Partial<Todo>;
   throw new Error('updateTodo: not implemented');
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+  const updatedArray: Todo[] = [...state];
+  updatedArray.filter(idToDelete => idToDelete.id === id);
+  return updatedArray
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+  const extractedTodo = state.find(item => item.id === id)
+  return extractedTodo
 }
