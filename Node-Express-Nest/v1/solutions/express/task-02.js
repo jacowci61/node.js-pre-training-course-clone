049c9873d7e69cfdf6d7375a9628aa7aa9856ea2
jacowci61 <jacowci61@gmail.com>
2026-09-07 23:06:53 +0300

    Completed task n2 EJS

---
+++ Node-Express-Nest/v1/solutions/express/task-02.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+  const active = req.query.active;
+
+ function isNumeric(str) {
+  return /^\d+$/.test(str);
+}
+
+  function isBoolean(str){
+    if (str === "true" || str === "false"){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
+  if(isNumeric(id) == false){
+    res.status(400).send("Error parsing user id");
+    return
+  }
+  if(isBoolean(active) == false){
+    res.status(400).send("Error parsing status");
+    return
+  }
+
+  res.send('User and status are correct');
+});
+
+const PORT = 3001; // different port than task1, in case both run at once
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
