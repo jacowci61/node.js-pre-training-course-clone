920c94a571141484e6a67ebd01a499d57c18a833
jacowci61 <jacowci61@gmail.com>
2026-09-07 17:33:16 +0300

    Completed task n1 in EJS

--- Node-Express-Nest/v1/solutions/express/task-01.js
+++ Node-Express-Nest/v1/solutions/express/task-01.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const app = express();
 
-// --- Middleware 1: Logger ---
-// TODO: log the HTTP method and URL of every incoming request
 function logger(req, res, next) {
   const method = req.method;
   const url = req.url;
   console.log('LOGGER:' + `${method} ${url}`); // fill this in
-  next(); // hands off control to the next middleware — don't forget this
+  next(); 
 }
 
-// --- Middleware 2: Timer ---
-// TODO: record a start time, then after the response finishes, log how long it took
 function timer(req, res, next) {
   const start = Date.now();
   res.on('finish', () =>
@@ -19,19 +15,14 @@ function timer(req, res, next) {
     const duration = Date.now() - start
     console.log('Request took ' + `${duration}`)
 });
-  // hint: res has a 'finish' event you can listen to
   next();
 }
 
-// --- Middleware 3: Custom header injector ---
-// TODO: add a custom header to every outgoing response, e.g. X-Powered-By-Me
 function headerInjector(req, res, next) {
   res.setHeader('Custom-header','Custom header 123');
   next();
 }
 
-// Register your middlewares here, in order:
-// app.use(...)
 
 app.get('/', (req, res) => {
   res.send('Middleware playground');
