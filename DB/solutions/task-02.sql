d2a577bb7b7229e989e324d10fd97415cda4dc3d
Nikita Rovinets <jacowci61@gmail.com>
2026-09-16 16:58:29 +0300

    Completed sql n2

--- DB/solutions/task-02.sql
+++ DB/solutions/task-02.sql
@@ -2,3 +2,16 @@ insert into users (id, name)
 values ("1", "Username")
 insert into todos (title, description, status, user_id)
 values ("TodoTitle", "TodoDescription", "IN_PROGRESS", 1)
+insert into todos (title, description, status, user_id)
+-- -------------------  
+values ("TodoTitle2", "TodoDescription", "IN_PROGRESS", 1) -- to have multiple todo rows
+-- -------------------  
+SELECT * FROM todos
+-- -------------------  
+update todos
+set
+todos.status = "COMPLETED"
+where id in (5) -- select specific todo by its id
+-- -------------------
+delete from todos
+where id in (5) -- select specific todo by its id
