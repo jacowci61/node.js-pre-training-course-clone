48f44f59437f4a5de1ad777401719ba148124e9f
Nikita Rovinets <jacowci61@gmail.com>
2026-09-16 17:30:55 +0300

    Completed SQL n4

--- DB/solutions/task-04.sql
+++ DB/solutions/task-04.sql
@@ -4,3 +4,7 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL,
     created_at TIMESTAMP default CURRENT_TIMESTAMP
 );
+
+ALTER TABLE todos 
+ADD user_id INT not null,
+foreign key(user_id) references users(id)
